6ecce2e39b723dc8b88a15162146084565a73d44
ByronLi <byronli@usc.edu>
2018-10-17 11:33:47 -0700

    login and signup logic

--- app/src/main/java/csci310/plshireus/org/kindler/Signup.java
+++ app/src/main/java/csci310/plshireus/org/kindler/Signup.java
@@ -22,46 +22,47 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.squareup.picasso.Picasso;
+import csci310.plshireus.org.kindler.objects.User;
 
-public class Login extends AppCompatActivity {
-
-    /**
-     * Id to identity READ_CONTACTS permission request.
-     */
-    private static final int REQUEST_READ_CONTACTS = 0;
-    private static final String TAG = "EmailPassword";
+public class Signup extends AppCompatActivity {
 
     /**
      * Keep track of the login task to ensure we can cancel it if requested.
      */
-    private UserLoginTask mAuthTask = null;
+    private UserRegisterTask mAuthTask = null;
     private FirebaseAuth firebaseAuth = null;
+    private DatabaseReference mDatabase;
 
     // UI references.
     private AutoCompleteTextView mEmailView;
+    private EditText mNameView;
     private EditText mPasswordView;
+    private EditText mPasswordCheckView;
     private View mProgressView;
     private View mLoginFormView;
-    private View createAccountView;
+    private View loginButtonView;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
-        // Set up the login form.
-        mEmailView = (AutoCompleteTextView) findViewById(R.id.email);
+        setContentView(R.layout.activity_signup);
 
-        //TODO: eliminate need to log in every time after testing
         firebaseAuth = FirebaseAuth.getInstance();
-        FirebaseAuth.getInstance().signOut();
+        mDatabase = FirebaseDatabase.getInstance().getReference();
 
-        ImageView loginLogo = (ImageView) findViewById(R.id.login_logo);
-        String tinderLogo = "https://firebasestorage.googleapis.com/v0/b/kindler-2e035.appspot.com/o/tinder-logo.png?alt=media&token=d104b4af-5344-4f87-b51a-9c5c2d9bede3";
-        Picasso.with(getBaseContext()).load(tinderLogo).into(loginLogo);
+        ImageView signupLogo = (ImageView) findViewById(R.id.signup_logo);
+        Picasso.with(getApplicationContext()).load(R.mipmap.ic_launcher).into(signupLogo);
 
+        mEmailView = (AutoCompleteTextView) findViewById(R.id.email);
+        mNameView = (EditText) findViewById(R.id.name);
         mPasswordView = (EditText) findViewById(R.id.password);
+        mPasswordCheckView = (EditText) findViewById(R.id.password_check);
+
         mPasswordView.setOnEditorActionListener(new TextView.OnEditorActionListener() {
             @Override
             public boolean onEditorAction(TextView textView, int id, KeyEvent keyEvent) {
@@ -83,25 +84,26 @@ public class Login extends AppCompatActivity {
 
         mLoginFormView = findViewById(R.id.login_form);
         mProgressView = findViewById(R.id.login_progress);
-        createAccountView = findViewById(R.id.link_signup);
-        createAccountView.setOnClickListener(new View.OnClickListener() {
+        loginButtonView = findViewById(R.id.link_login);
+        loginButtonView.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), Signup.class);
+                // Start the Signup activity
+                Intent intent = new Intent(getApplicationContext(), Login.class);
                 startActivity(intent);
             }
         });
     }
 
-    @Override
-    public void onBackPressed() {
-        // disable going back to the MainActivity
-        moveTaskToBack(true);
-    }
+//    @Override
+//    public void onBackPressed() {
+//        // disable going back to the MainActivity
+//        moveTaskToBack(true);
+//    }
 
     /**
-     * Attempts to sign in or register the account specified by the login form.
+     * Attempts to register the account specified by the login form.
      * If there are form errors (invalid email, missing fields, etc.), the
      * errors are presented and no actual login attempt is made.
      */
@@ -114,10 +116,14 @@ public class Login extends AppCompatActivity {
         // Reset errors.
         mEmailView.setError(null);
         mPasswordView.setError(null);
+        mNameView.setError(null);
+        mPasswordCheckView.setError(null);
 
         // Store values at the time of the login attempt.
         String email = mEmailView.getText().toString();
         String password = mPasswordView.getText().toString();
+        String name = mNameView.getText().toString();
+        String passwordCheck = mPasswordCheckView.getText().toString();
 
         boolean cancel = false;
         View focusView = null;
@@ -130,6 +136,7 @@ public class Login extends AppCompatActivity {
         }
 
         // Check for a valid email address.
+
         if (TextUtils.isEmpty(email)) {
             mEmailView.setError(getString(R.string.error_field_required));
             focusView = mEmailView;
@@ -139,6 +146,20 @@ public class Login extends AppCompatActivity {
             focusView = mEmailView;
             cancel = true;
         }
+        else if (name.length() < 2){
+            mNameView.setError("Name must be at least 2 characters");
+            focusView = mNameView;
+            cancel = true;
+        }
+
+        //check that both passwords are the same
+
+        if (!password.equals(passwordCheck)){
+            mPasswordCheckView.setError(getString(R.string.error_password_notmatch));
+            focusView = mPasswordCheckView;
+            cancel = true;
+        }
+
 
         if (cancel) {
             // There was an error; don't attempt login and focus the first
@@ -149,7 +170,7 @@ public class Login extends AppCompatActivity {
             // perform the user login attempt.
             toggleKeyboard(this);
             showProgress(true);
-            mAuthTask = new UserLoginTask(email, password, this);
+            mAuthTask = new UserRegisterTask(name, email, password);
             mAuthTask.execute((Void) null);
         }
     }
@@ -165,8 +186,8 @@ public class Login extends AppCompatActivity {
     /**
      * Shows the progress UI and hides the login form.
      */
-    @SuppressWarnings("Duplicates")
     @TargetApi(Build.VERSION_CODES.HONEYCOMB_MR2)
+    @SuppressWarnings("Duplicates")
     private void showProgress(final boolean show) {
         // On Honeycomb MR2 we have the ViewPropertyAnimator APIs, which allow
         // for very easy animations. If available, use these APIs to fade-in
@@ -203,55 +224,60 @@ public class Login extends AppCompatActivity {
      * Represents an asynchronous login/registration task used to authenticate
      * the user.
      */
-    public class UserLoginTask extends AsyncTask<Void, Void, Boolean> {
+    public class UserRegisterTask extends AsyncTask<Void, Void, Boolean> {
 
+        private final String mName;
         private final String mEmail;
         private final String mPassword;
-        private final Activity mActivity;
 
-        UserLoginTask(String email, String password, Activity activity) {
+        UserRegisterTask(String name, String email, String password) {
             mEmail = email;
             mPassword = password;
-            mActivity = activity;
+            mName = name;
         }
 
         @Override
         protected Boolean doInBackground(Void... params) {
 
-            FirebaseUser user = null;
-
             try {
-                firebaseAuth.signInWithEmailAndPassword(mEmail, mPassword)
-                        .addOnCompleteListener(Login.this, new OnCompleteListener<AuthResult>() {
+
+                firebaseAuth.createUserWithEmailAndPassword(mEmail, mPassword)
+                        .addOnCompleteListener(Signup.this, new OnCompleteListener<AuthResult>() {
                             @Override
                             public void onComplete(@NonNull Task<AuthResult> task) {
                                 if (task.isSuccessful()) {
                                     // Sign in success, update UI with the signed-in user's information
-                                    System.out.println("Log in success");
-                                    Log.d(TAG, "signInWithEmail:success");
+                                    Log.d("Debug", "createUserWithEmail:success");
+                                    FirebaseUser user = firebaseAuth.getCurrentUser();
+                                    String userId = user.getUid();
+                                    User uObj = new User(mName, mEmail);
+                                    System.out.print(userId);
+                                    mDatabase.child("Users").child(userId).setValue(uObj);
+                                    Toast.makeText(Signup.this, "Success",
+                                            Toast.LENGTH_LONG).show();
+                                    //mDatabase.child("Users").setValue(2);
+
                                 } else {
                                     // If sign in fails, display a message to the user.
-                                    Log.w(TAG, "signInWithEmail:failure", task.getException());
-                                    Toast.makeText(Login.this, "Authentication failed.",
-                                            Toast.LENGTH_SHORT).show();
+                                    Log.w("Debug", "createUserWithEmail:failure", task.getException());
+                                    //Toast.makeText(Signup.this, "Authentication failed.",
+                                    //Toast.LENGTH_SHORT).show();
 
                                 }
+
                                 // ...
                             }
                         });
 
-            //Wait for response from Firebase
                 Thread.sleep(2500);
             } catch (InterruptedException e) {
                 return false;
             }
 
-            user = FirebaseAuth.getInstance().getCurrentUser();
-
+            FirebaseUser user = firebaseAuth.getCurrentUser();
             if (user != null) {
                 return true;
             } else {
-                Log.d("Error", "Issue with retrieving response from Firebase. Try login again");
                 return false;
             }
 
@@ -263,15 +289,12 @@ public class Login extends AppCompatActivity {
             showProgress(false);
 
             if (success) {
-                System.out.println("finish");
                 Intent homeScreen = new Intent(getApplicationContext(), MainActivity.class);
                 startActivity(homeScreen);
                 finish();
-
             } else {
-                mPasswordView.setError(getString(R.string.error_incorrect_password));
-                mPasswordView.requestFocus();
-                toggleKeyboard(mActivity);
+                mEmailView.setError(getString(R.string.user_exists));
+                mEmailView.requestFocus();
             }
         }
 
@@ -282,7 +305,7 @@ public class Login extends AppCompatActivity {
         }
     }
 
-    public void toggleKeyboard(Activity activity){
+    public static void toggleKeyboard(Activity activity){
         InputMethodManager imm = (InputMethodManager) activity.getSystemService(Activity.INPUT_METHOD_SERVICE);
         if (imm.isActive()){
             // Hide keyboard
@@ -292,6 +315,5 @@ public class Login extends AppCompatActivity {
             //imm.toggleSoftInput(0, InputMethodManager.HIDE_IMPLICIT_ONLY);
         }
     }
-
 
 }
