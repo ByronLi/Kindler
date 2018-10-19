379a97e1081fa48736641345a4bec1e48b9dc949
Jonathan Yao <jonathanyao@Jonathans-MacBook-Pro.local>
2018-10-19 00:04:22 -0700

    swiping for feed

---
+++ app/src/main/java/csci310/plshireus/org/kindler/FeedAdapter.java
@@ -0,0 +1,42 @@
+package csci310.plshireus.org.kindler;
+import android.view.View;
+import com.huxq17.swipecardsview.BaseCardAdapter;
+import java.util.List;
+import android.content.Context;
+import android.widget.ImageView;
+import android.widget.TextView;
+import com.squareup.picasso.Picasso;
+
+
+public class FeedAdapter extends BaseCardAdapter{
+    private List<Model> modelList;
+    private Context context;
+
+    public FeedAdapter(List<Model> modelList, Context context){
+        this.modelList = modelList;
+        this.context = context;
+
+    }
+
+    public int getCount(){
+        return modelList.size();
+
+    }
+
+    public int getCardLayoutId(){
+        return R.layout.feed_item;
+    }
+
+    public void onBindData(int position, View cardview){
+        if(modelList == null|| modelList.size() ==0){
+            return;
+        }
+        ImageView imageview = (ImageView) cardview.findViewById(R.id.imageView);
+        TextView textview = (TextView) cardview.findViewById(R.id.textView);
+        Model model = modelList.get(position);
+        textview.setText(model.getTitle());
+        Picasso.with(context).load(model.getImage()).into(imageview);
+
+    }
+
+}
--- app/src/main/java/csci310/plshireus/org/kindler/MainActivity.java
+++ app/src/main/java/csci310/plshireus/org/kindler/MainActivity.java
@@ -1,13 +1,31 @@
 package csci310.plshireus.org.kindler;
-
+import com.huxq17.swipecardsview.SwipeCardsView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import java.util.List;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
+    private SwipeCardsView swipecardsView;
+    private List<Model> modelList = new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        swipecardsView = (SwipeCardsView)findViewById(R.id.swipeCardsView);
+        swipecardsView.retainLastCard(false);
+        swipecardsView.enableSwipe(true);
+        getData();
+    }
+
+    private void getData(){
+        modelList.add(new Model("Harry Potter 1","https://vignette.wikia.nocookie.net/harrypotter/images/0/0e/Philostone.jpg/revision/latest?cb=20180318153750"));
+        modelList.add(new Model("50 shades of grey","https://m.media-amazon.com/images/M/MV5BMjE1MTM4NDAzOF5BMl5BanBnXkFtZTgwNTMwNjI0MzE@._V1_.jpg"));
+        modelList.add(new Model("To kill a mockingbird", "https://images-na.ssl-images-amazon.com/images/I/71FxgtFKcQL.jpg"));
+        modelList.add(new Model("Hunger games", "https://upload.wikimedia.org/wikipedia/en/thumb/3/39/The_Hunger_Games_cover.jpg/220px-The_Hunger_Games_cover.jpg"));
+        FeedAdapter cardAdapter = new FeedAdapter(modelList,this);
+        swipecardsView.setAdapter(cardAdapter);
     }
 }
