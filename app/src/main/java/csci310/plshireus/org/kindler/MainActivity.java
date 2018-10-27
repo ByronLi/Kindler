f5030fac400790b9e8f15a1277a5df49d542becf
Jonathan Yao <jonathanyao@Jonathans-MacBook-Pro.local>
2018-10-27 16:53:51 -0700

    feed now pulls from database successfully

--- app/src/main/java/csci310/plshireus/org/kindler/MainActivity.java
+++ app/src/main/java/csci310/plshireus/org/kindler/MainActivity.java
@@ -16,6 +16,7 @@ import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import java.util.concurrent.CountDownLatch;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -42,8 +43,21 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Intent createBook = new Intent(getApplicationContext(), CreateBook.class);
                 startActivity(createBook);
+
+                //swipecardsView.setAdapter(cardAdapter); //this is what shows the feed
             }
         });
+
+        Button fab2 = (Button) findViewById(R.id.refreshButton);
+
+        fab2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                swipecardsView.setAdapter(cardAdapter); //this is what shows the feed
+            }
+        });
+
 
     }
 
@@ -60,7 +74,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
                 // This method is called once with the initial value and again
-                // whenever data at this location is updated.
+                // whenever data at this location is updated
 
                     for(DataSnapshot snapshot: dataSnapshot.getChildren()){
 
@@ -70,26 +84,28 @@ public class MainActivity extends AppCompatActivity {
                         Model curr_model = new Model(tit, img);
                         System.out.println("-------------@@@@@@--------------");
                         System.out.println(curr_model.title);
+                        System.out.println(curr_model.imageURL);
 
                         modelList.add(curr_model);
-                        modelList.add(new Model());
+                        cardAdapter.updateList(modelList);
+                        swipecardsView.postInvalidate();
 
                     }
             }
 
             @Override
             public void onCancelled(DatabaseError error) {
-
+                System.out.println("----------########-------" + error);
             }
         });
 
-        modelList.add(new Model("JKROWLING","fantasy", "https://vignette.wikia.nocookie.net/harrypotter/images/0/0e/Philostone.jpg/revision/latest?cb=20180318153750", "fd", "100", "Harry Potter"));
+       // modelList.add(new Model("JKROWLING","fantasy", "https://vignette.wikia.nocookie.net/harrypotter/images/0/0e/Philostone.jpg/revision/latest?cb=20180318153750", "fd", "100", "Harry Potter"));
 
-        modelList.add(new Model("Harry Potter 1","https://vignette.wikia.nocookie.net/harrypotter/images/0/0e/Philostone.jpg/revision/latest?cb=20180318153750"));
+       // modelList.add(new Model("Harry Potter 1","https://vignette.wikia.nocookie.net/harrypotter/images/0/0e/Philostone.jpg/revision/latest?cb=20180318153750"));
 
-        modelList.add(new Model("50 shades of grey","https://m.media-amazon.com/images/M/MV5BMjE1MTM4NDAzOF5BMl5BanBnXkFtZTgwNTMwNjI0MzE@._V1_.jpg"));
-        modelList.add(new Model("To kill a mockingbird", "https://images-na.ssl-images-amazon.com/images/I/71FxgtFKcQL.jpg"));
-        modelList.add(new Model("Hunger games", "https://upload.wikimedia.org/wikipedia/en/thumb/3/39/The_Hunger_Games_cover.jpg/220px-The_Hunger_Games_cover.jpg"));
+      //  modelList.add(new Model("50 shades of grey","https://m.media-amazon.com/images/M/MV5BMjE1MTM4NDAzOF5BMl5BanBnXkFtZTgwNTMwNjI0MzE@._V1_.jpg"));
+       // modelList.add(new Model("To kill a mockingbird", "https://images-na.ssl-images-amazon.com/images/I/71FxgtFKcQL.jpg"));
+       // modelList.add(new Model("Hunger games", "https://upload.wikimedia.org/wikipedia/en/thumb/3/39/The_Hunger_Games_cover.jpg/220px-The_Hunger_Games_cover.jpg"));
 
         cardAdapter = new FeedAdapter(modelList,this);
         swipecardsView.setAdapter(cardAdapter);
