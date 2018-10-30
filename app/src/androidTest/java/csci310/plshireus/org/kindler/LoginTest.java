f0cf0fdd7bcd6d1e695c706d2065e79b285f47b8
jerrytejada <jmtejada@usc.edu>
2018-10-29 21:38:58 -0700

    LoginTest File

---
+++ app/src/androidTest/java/csci310/plshireus/org/kindler/LoginTest.java
@@ -0,0 +1,19 @@
+package csci310.plshireus.org.kindler;
+
+//import static org.mockito.Mockito.*;
+
+public class LoginTest {
+
+//        @Mock
+//        MyDatabase databaseMock;
+//
+//        @Rule public MockitoRule mockitoRule = MockitoJUnit.rule();
+//
+//        @Test
+//        public void testQuery() {
+//            ClassToTest t = new ClassToTest(databaseMock);
+//            boolean check = t.query("* from t");
+//            assertTrue(check);
+//            verify(databaseMock).query("* from t");
+//        }
+}
